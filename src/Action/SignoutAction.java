1b2dc187ee855812b43f76c657beff922aefe9a1
Einere <kjwsx23@gamil.com>
2018-06-06 18:04:14 +0900

    Modify signoutAction

--- src/Action/SignoutAction.java
+++ src/Action/SignoutAction.java
@@ -16,6 +16,6 @@ public class SignoutAction implements CommandAction {
         session.removeAttribute("identity");
         session.removeAttribute("dest");
         session.removeAttribute("lectureName");
-        return "home.jsp";
+        return "login.jsp";
     }
 }
